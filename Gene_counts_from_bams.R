0b66a6e89dd5b39f2c26548ec3cdef80acb03f57
damargal <damargal@gmail.com>
2017-03-22 10:44:23 +0000

    update information

--- Gene_counts_from_bams.R
+++ Gene_counts_from_bams.R
@@ -1,7 +1,8 @@
 #' Get count of fragments assgined to each gene
 #' 
-#' Get raw feature counts assgined to each gene
+#' Get raw feature counts assgined to each gene 
 #' in group of bam files using featureCounts (Rsubread)
+#' 
 #' 
 #'    RNA-seq workflow - gene-level exploratory analysis and differential expression
 #'    https://www.bioconductor.org/help/course-materials/2016/CSAMA/lab-3-rnaseq/rnaseq_gene_CSAMA2016.html#summarizing-an-rna-seq-experiment-as-a-count-matrix
@@ -27,9 +28,9 @@ gene_counts <- function(samples=sampleFile,bams=bamfiles,gtf=gtffile){
   seqinfo(bamfiles[1])
   
   #create a matrix with read/fragment counts assigned to each gene
-  #to include more options for counting reads, such as:
-  # library specificity
-  # to handgle multiple mapping reads
+  #TODO: to include more options for counting reads, such as:
+  #   - library specificity
+  #   - to handgle multiple mapping reads
   # ...
   fc <- featureCounts(files=filenames, 
                       annot.ext=gtffile, 
